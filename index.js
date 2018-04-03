b32557fc783af8ad43296e373d9a099bd0e688ea
dshtem@gmail.com <dshtem@gmail.com>
2018-04-03 21:59:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function happyHolidays() {
   return "Happy holidays!";
-}
+};
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
-}
+};
 function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
-}
+};
 function holidayCountdown(holiday, number) {
   return `It\'s ${number} days until ${holiday}!`;
-}
+};
